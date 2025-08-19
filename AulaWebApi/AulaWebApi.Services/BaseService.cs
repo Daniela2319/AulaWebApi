fe7406a2f538e0fe472a65c9887d16bfdff5127d
Daniela2319 <frankvt04@gmail.com>
2025-08-19 10:41:07 -0300

    feat: criado o projeto para as services

---
+++ AulaWebApi/AulaWebApi.Services/BaseService.cs
@@ -0,0 +1,12 @@
+﻿namespace AulaWebApi.Services
+{
+    public class BaseService
+    {
+        public void Create() { }
+        public void Read() { }
+        public void Update() { }
+        public void Delete() { }
+        public void ReadById() { }
+
+    }
+}
