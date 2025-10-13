d2cfd7272d9912bdcde21457fb2a9c21950aad7f
Daniela2319 <frankvt04@gmail.com>
2025-10-13 09:37:54 -0300

    feat(data-base): created the connnection with database

--- AulaWebApi/AulaWebApi.Models/BaseModel.cs
+++ AulaWebApi/AulaWebApi.Models/BaseModel.cs
@@ -3,11 +3,11 @@
     public class BaseModel
     {
         public int Id { get; set; }
-        public DateTime CreatedAd { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         public override string ToString()
         {
-            return $"{this.Id} - {this.CreatedAd}";
+            return $"{this.Id} - {this.CreatedAt}";
         }
     }
 }
